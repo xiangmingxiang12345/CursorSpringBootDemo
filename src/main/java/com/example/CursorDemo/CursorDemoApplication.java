70b178e263628c4146c8647292d03d44138d1e6d
userName <userEmail>
2025-06-23 09:02:50 +0800

    初始化

---
+++ src/main/java/com/example/CursorDemo/CursorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CursorDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursorDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/CursorDemo/CursorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CursorDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
